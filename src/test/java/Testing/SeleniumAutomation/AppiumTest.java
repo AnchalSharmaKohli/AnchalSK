49148f8609d8c02ffe9d366e2c6906445b152244
anchal <anchalkohli3005@gmail.com>
2023-05-10 18:33:08 +0100

    My first push

---
+++ src/test/java/Testing/SeleniumAutomation/AppiumTest.java
@@ -0,0 +1,14 @@
+package Testing.SeleniumAutomation;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void webappdriver()
+	{
+		System.out.println("webappdriver");
+	}
+	
+
+}
---
+++ src/test/java/Testing/SeleniumAutomation/SeleniumTest.java
@@ -0,0 +1,19 @@
+package Testing.SeleniumAutomation;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void webdriver()
+	{
+		System.out.println("webdriver");
+	}
+	@Test
+	public void webseldriver()
+	{
+		System.out.println("webseldriver");
+	}
+	
+
+}
